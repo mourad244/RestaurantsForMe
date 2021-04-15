4f1060c213ec8555019049c6008c2d0514f2ddbb
mourad244 <mouradvilla2@gmail.com>
2021-04-15 22:41:07 +0000

    fetch data from backend and display images

--- app/src/main/java/com/hfad/restaurantsforme/SecondActivity.java
+++ app/src/main/java/com/hfad/restaurantsforme/SecondActivity.java
@@ -2,14 +2,10 @@ package com.hfad.restaurantsforme;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
